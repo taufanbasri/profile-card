dbfc8cc1ba381432a3295d19edd0d080e8892247
taufanbasri <taufanprasetyobasri@gmail.com>
2022-04-02 21:38:26 +0700

    Setup Tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,17 @@
 module.exports = {
   content: [],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'kumbh': ['Kumbh Sans', 'sans-serif']
+      },
+      colors: {
+        'dark-cyan': 'hsl(185, 75%, 39%)',
+        'desaturated-blue': 'hsl(229, 23%, 23%)',
+        'grayish-blue': 'hsl(227, 10%, 46%)',
+        'ark-gray': 'hsl(0, 0%, 59%)',
+      }
+    },
   },
   plugins: [],
 }
